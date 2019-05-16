04c35b8863b9ecf2d82e9cae231c89a5b3cc43af
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-16 15:04:37 -0600

    added Artist and Songs
    Added the models and controllers for artists and songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_203802) do
+ActiveRecord::Schema.define(version: 2019_05_16_210320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "billboards", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,13 @@ ActiveRecord::Schema.define(version: 2019_05_16_203802) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  add_foreign_key "songs", "artists"
 end
