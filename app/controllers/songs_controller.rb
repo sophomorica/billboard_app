b5b36228ba2c576e2ede9f81b4ed6bc6827e243b
Patrick Lamoureux <plamoureux84@gmail.com>
2019-05-17 16:54:17 -0600

    i can add songs to an artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,33 @@
 class SongsController < ApplicationController
+  before_action :set_artist
   def index
+    @songs = Song.all
   end
 
-  def show
-  end
+ 
+  
 
   def edit
   end
 
   def new
+    @song = @artist.songs.new
+  end
+
+  def create
+    @song = @artist.songs.new(song_params)
+    if @song.save
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+  def song_params
+    params.require(:song).permit(:on_billboard, :name)
   end
 end
